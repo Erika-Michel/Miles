a3ec582b0e0141bcac85edc723c9ba27e0fc8520
Erika-Michel <erikamichel174@gmail.com>
2021-05-24 19:14:50 +0200

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketInKopecks = 3030503;
+        int kopecksProMile = 2000;
+        int miles = ticketInKopecks / kopecksProMile;
+        System.out.println(miles);
+    }
+}
